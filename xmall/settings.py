5d57aae4daab4495133c52e99882c456bc81b4af
hqc <2996144066@qq.com>
2020-03-24 10:22:51 +0800

    提交虚拟环境所需的包、修改数据库配置

--- xmall/settings.py
+++ xmall/settings.py
@@ -93,12 +93,25 @@ WSGI_APPLICATION = 'xmall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_xmall',
+        'USER': 'newuser1',
+        'PASSWORD': '123456',
+        'HOST': '47.102.210.128',
+        'PORT': '3306'
     }
+
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
